574f1ac45beab42f7d7b8be857122900a82ddfc5
Cristian Iusca <ciusca@c2r6p1.42firenze.it>
2023-12-04 18:59:54 +0100

    populate maps

---
+++ so_long/handle_map.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_map.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ciusca <ciusca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 16:57:29 by ciusca            #+#    #+#             */
+/*   Updated: 2023/12/04 18:59:42 by ciusca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include "get_next_line/get_next_line.c"
+#include "get_next_line/get_next_line_utils.c"
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+
+void	display_map(t_data *mlx, char **matrix)
+{
+	int	cols;
+	int	rows;
+
+	cols = 0;
+	rows = 0;
+	/*while (matrix[cols] != NULL)
+	{
+		while (matrix[cols][rows] != 0)
+		{
+			printf("%s\n", matrix[rows]);
+			rows++;
+		}
+		rows = 0;
+		printf("%s\n",matrix[cols]);
+		cols++;
+	}*/
+	//printf("cols = %d \n rows = %d\n", cols, rows);
+	mlx->win = mlx_new_window(mlx->mlx, 64 * 5, 64 * 5, "test");
+}
+
+void	populate_map(t_data *mlx)
+{
+	char	**matrix;
+	char	*line;
+	int		fd;
+	int		count;
+	int		i;
+
+	i = 0;
+	count = 0;
+	fd = open("Maps/secondMap.ber", O_RDONLY);
+	if (fd == -1)
+		exit(0);
+	while (get_next_line(fd) != 0)
+		count++;
+	matrix = malloc(sizeof(char) * count + 1);
+	matrix[count] = NULL;
+	close(fd);
+	fd = open("Maps/secondMap.ber", O_RDONLY);
+	while (i < count)
+	{
+		line = get_next_line(fd);
+		line[strlen(line) - 1] = 0;
+		matrix[i] = line;
+		i++;
+	}
+	close(fd);
+	display_map(mlx, matrix);
+}
--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: ciusca <ciusca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 16:00:40 by ciusca            #+#    #+#             */
-/*   Updated: 2023/12/04 16:16:06 by ciusca           ###   ########.fr       */
+/*   Updated: 2023/12/04 17:39:47 by ciusca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,50 +16,20 @@
 #include "so_long.h"
 #include <fcntl.h>
 #include <unistd.h>
-
-void	get_map_size(t_data *mlx)
-{
-	int		i;
-	int		rows;
-	int		cols;
-	char	*buffer;
-	int		fd;
-	int		bytes_read;
-
-	buffer = calloc(20000, sizeof(char));
-	i = -1;
-	fd = open("Maps/mainMap.ber", O_RDONLY);
-	if (fd == -1)
-	{
-		printf("map not found");
-		exit(0);
-	}
-	bytes_read = read(fd, buffer, 20000);
-	if (bytes_read == -1)
-	{
-		printf("error reading file");
-		exit(0);
-	}
-	buffer[bytes_read] = 0;
-	while (buffer[++i] != '\n')
-		cols++;
-	i = -1;
-	while (buffer[++i] != 0)
-		if (buffer[i] == '\n')
-			rows ++;
-	rows++;
-	mlx->win = mlx_new_window(mlx->mlx, 64 * 5, 64 * 5, "test");
-}
+#include "handle_map.c"
+#include "player_movment.c"
 
 void	create_img(t_data *mlx)
 {
-	char	*relative_path = "calci.xpm";
+	char	*relative_path;
 	int		width;
 	int		height;
 
+	relative_path = "calci.xpm";
 	width = 1080;
 	height = 720;
-	mlx->sprites[0].img = mlx_xpm_file_to_image(mlx->mlx, relative_path, &width, &height);
+	mlx->sprites[0].img = mlx_xpm_file_to_image(mlx->mlx, relative_path,
+			&width, &height);
 	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->sprites[0].img, 0, 0);
 }
 
@@ -79,8 +49,9 @@ void	player_movment(t_data *mlx, int horizontal, int vertical)
 	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->sprites[0].img, x, y);
 }
 
-int	onKeydown(int key, t_data *img)
-{
+int	on_keydown(int key, t_data *img)
+{if (key == 65307)
+		exit(0);
 	printf("%d\n", key);
 	if (key == 65307)
 		exit(0);
@@ -95,32 +66,14 @@ int	onKeydown(int key, t_data *img)
 	return (0);
 }
 
-int	onMouseMove(int pos)
-{
-	printf("%d\n", pos);
-	return (0);
-}
-
-int	get_mouse(void *img)
-{
-	int		x;
-	int		y;
-	t_data	*data;
-
-	data = (t_data *)img;
-	mlx_mouse_get_pos(data->mlx, data->win, &x, &y);
-	printf("x: %d\ny: %d", x, y);
-	return (0);
-}
-
 int	main(void)
 {
 	t_data	img;
 	int		fd;
 
 	img.mlx = mlx_init();
-	get_map_size(&img);
+	populate_map(&img);
 	create_img(&img);
-	mlx_key_hook(img.win, onKeydown, &img);
+	mlx_key_hook(img.win, on_keydown, &img);
 	mlx_loop(img.mlx);
 }

f61a90ad51326cd2e8b453f36e0707f040b75640
Cristian Iusca <ciusca@c2r6p9.42firenze.it>
2023-12-05 18:12:31 +0100

    to be fixed the map matrix

--- so_long/handle_map.c
+++ so_long/handle_map.c
@@ -6,7 +6,7 @@
 /*   By: ciusca <ciusca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 16:57:29 by ciusca            #+#    #+#             */
-/*   Updated: 2023/12/04 18:59:42 by ciusca           ###   ########.fr       */
+/*   Updated: 2023/12/05 18:12:22 by ciusca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,22 @@
 #include <stdio.h>
 #include <string.h>
 
-void	display_map(t_data *mlx, char **matrix)
+void	display_map(t_data *mlx, char **matrix, int count)
 {
 	int	cols;
 	int	rows;
 
 	cols = 0;
 	rows = 0;
-	/*while (matrix[cols] != NULL)
+	/*while (cols < count)
 	{
 		while (matrix[cols][rows] != 0)
-		{
-			printf("%s\n", matrix[rows]);
 			rows++;
-		}
-		rows = 0;
-		printf("%s\n",matrix[cols]);
+		printf("%s\n", matrix[cols]);
 		cols++;
 	}*/
-	//printf("cols = %d \n rows = %d\n", cols, rows);
-	mlx->win = mlx_new_window(mlx->mlx, 64 * 5, 64 * 5, "test");
+	//printf("cols = %d \n rows = %d", cols, rows);
+	mlx->win = mlx_new_window(mlx->mlx, 64 * 6, 64 * 6, "test");
 }
 
 void	populate_map(t_data *mlx)
@@ -44,27 +40,28 @@ void	populate_map(t_data *mlx)
 	char	**matrix;
 	char	*line;
 	int		fd;
-	int		count;
 	int		i;
 
 	i = 0;
-	count = 0;
 	fd = open("Maps/secondMap.ber", O_RDONLY);
 	if (fd == -1)
 		exit(0);
 	while (get_next_line(fd) != 0)
-		count++;
-	matrix = malloc(sizeof(char) * count + 1);
-	matrix[count] = NULL;
+		i++;
+	matrix = malloc(sizeof(char) * i + 1);
+	matrix[i] = NULL;
 	close(fd);
 	fd = open("Maps/secondMap.ber", O_RDONLY);
-	while (i < count)
+	i--;
+	while (i >= 0)
 	{
 		line = get_next_line(fd);
 		line[strlen(line) - 1] = 0;
 		matrix[i] = line;
-		i++;
+		//printf("%s\n", matrix[i]);
+		i--;
 	}
+	//printf("matrix 0 = %s \nmatrix last = %s", matrix[0], matrix[4]);
 	close(fd);
-	display_map(mlx, matrix);
+	display_map(mlx, matrix, i);
 }

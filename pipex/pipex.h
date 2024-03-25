cbba616b057e3c4bda06fdb352f00917aa1bd897
Cristian Iusca <ciusca@c1r3p4.42firenze.it>
2024-03-25 19:10:47 +0100

    finished mandatory
    to do
    
    implement here_doc
    test bonus

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,14 +6,13 @@
 /*   By: ciusca <ciusca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 11:19:32 by ciusca            #+#    #+#             */
-/*   Updated: 2024/03/07 10:21:39 by ciusca           ###   ########.fr       */
+/*   Updated: 2024/03/25 18:10:03 by ciusca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# define stdint 0
 # include "libft/libft.h"
 # include <sys/wait.h>
 
@@ -28,6 +27,8 @@ typedef struct s_args
 	char	**argv;
 	char	**envp;
 	char	**cmd_path;
+	int		argc;
+	int		fds[2];
 	t_files	file;
 }				t_args;
 
@@ -38,6 +39,6 @@ int	check_command(char **argv, char **path, t_args *pipex);
 int	manage_files(char **argv, int argc, t_args *pipex);
 
 /* executing commands */
-int	execute_command(char **argv, char **envp, t_args *pipex);
+int	execute_command(t_args *pipex);
 
 #endif
